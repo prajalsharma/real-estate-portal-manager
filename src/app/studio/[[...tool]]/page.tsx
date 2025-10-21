25004299b49a885eb058d3634c511b1fb0b5123e
DarthPrajal <prajalsharma1120@gmail.com>
2025-10-21 19:03:26 +0530

    chore: remove conflicting optional catch-all studio route

--- src/app/studio/[[...tool]]/page.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Embedded Sanity Studio route
- */
-import { NextStudio } from 'next-sanity/studio'
-import config from '@/../sanity/sanity.config'
-
-export default function StudioPage() {
-  return <NextStudio config={config} />
-}
