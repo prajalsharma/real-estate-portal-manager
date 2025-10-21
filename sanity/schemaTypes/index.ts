bbaa26ca999c57a694c8996e87abbda07c9a050b
DarthPrajal <prajalsharma1120@gmail.com>
2025-10-21 15:59:28 +0530

    feat: add Sanity schema types index

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,14 @@
+import property from './property'
+import agent from './agent'
+import blogPost from './blogPost'
+import category from './category'
+import location from './location'
+
+export const schemaTypes = [
+  // Documents
+  property,
+  agent,
+  blogPost,
+  category,
+  location,
+]
