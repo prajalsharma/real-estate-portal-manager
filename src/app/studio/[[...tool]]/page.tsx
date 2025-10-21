146935a9cc56a69f79937020f2c42557ca526329
DarthPrajal <prajalsharma1120@gmail.com>
2025-10-21 18:53:44 +0530

    fix: correct import path to sanity/sanity.config using alias from src/app/studio route

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,9 +1,8 @@
 /**
  * Embedded Sanity Studio route
- * Handles all /studio/* routes using Next.js catch-all routing
  */
 import { NextStudioPage } from 'next-sanity/studio'
-import config from '../../../../../sanity/sanity.config'
+import config from '@/../sanity/sanity.config'
 
 export default function StudioPage() {
   return <NextStudioPage config={config} />
