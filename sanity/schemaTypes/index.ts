da59967644918d245237679f55e7dafdfda93bdf
DarthPrajal <prajalsharma1120@gmail.com>
2025-10-22 14:16:22 +0530

    chore(schemaTypes): keep only property schema in Sanity for a single unified All Properties section as requested

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,14 +1,5 @@
 import property from './property'
-import agent from './agent'
-import blogPost from './blogPost'
-import category from './category'
-import location from './location'
 
 export const schemaTypes = [
-  // Documents
   property,
-  agent,
-  blogPost,
-  category,
-  location,
 ]
